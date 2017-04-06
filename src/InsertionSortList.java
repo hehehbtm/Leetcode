6ed157fbdd8bb2ba46cf5f427a85756e8989dc74
hehehbtm <1104324732@qq.com>
2017-04-06 12:32:06 +0800

    链表相加

--- src/InsertionSortList.java
+++ src/InsertionSortList.java
@@ -1,11 +1,6 @@
 /**
  * Created by admin on 2017/4/6.
  */
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
 public class InsertionSortList {
     public ListNode insertionSortList(ListNode head) {
 
