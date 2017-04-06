f5e04b4a39c17d7a2f4e905f01ec8f65c53603f9
hehehbtm <1104324732@qq.com>
2017-04-06 11:37:07 +0800

    链表相加

---
+++ src/InsertionSortList.java
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 2017/4/6.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+
+        return null;
+    }
+}
--- src/Test1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by admin on 2017/4/6.
- */
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("Test1");
-    }
-}
